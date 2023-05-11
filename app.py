dcb065ea84c852d4297fac2b89a51ef7e680f45e
PavlosDaratzis2 <pauldaratzis1997@gmail.com>
2023-05-11 16:30:56 +0300

    epitelous

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # BEGIN CODE HERE
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, json
 from flask_pymongo import PyMongo
 from flask_cors import CORS
 from pymongo import TEXT
@@ -12,83 +12,56 @@ mongo = PyMongo(app)
 mongo.db.products.create_index([("name", TEXT)])
 
 
-
-
-
 @app.route("/search", methods=["GET"])
 def search():
     # BEGIN CODE HERE
 
     search_text = request.args.get('name', '')
-    
+
     if not search_text:
         return jsonify([])
-    
-
-    results = mongo.db.products.find({"name": {"$regex": search_text, "$options": "i"}}).sort('price')
+    results = mongo.db.products.find({"name": {"$regex": search_text, "$options": "i"}}).sort('price',-1)
 
     response = []
 
     for result in results:
         response.append({
-            'id':result['id'],
+            'id': str(result['_id']),
             'name': result['name'],
             'production_year': result['production_year'],
             'price': result['price'],
             'color': result['color'],
             'size': result['size']
         })
-    
-    
-
-    response.reverse()
+    # response.reverse()
     json_string = json.dumps(response, sort_keys=False)
     return json_string
 
-     # END CODE HERE
+    # END CODE HERE
 
 
 @app.route("/add-product", methods=["POST"])
 def add_product():
     # BEGIN CODE HERE
-    # new_person = {}
-    # new_person["id"] = request.args.get('id')
-    # new_person["name"] = request.args.get('name')
-    # new_person["production_year"] = request.args.get('production_year')
-    # new_person["price"] = request.args.get('price')
-    # new_person["color"] = request.args.get('color')
-    # new_person["size"] = request.args.get('size')
-    # print(new_person[id])
-    # # print(new_person[id])
-    # # print(new_person[id])
-    # # print(new_person[id])
-    # # print(new_person[id])
-    
-    # # the find One returns the first document that matches your query criteria or null
-    # exists = mongo.db.products.find_one({"name": new_person["name"]})
-    # if exists is None:
-    #     mongo.db.products.insert_one(new_person)
-    #     return "Addition Made"
-    # else:
-    #     mongo.db.products.update_one({"name": new_person["name"]}, {"$set": {"id": new_person["id"],"production_year": new_person["production_year"],"price": new_person["price"],"color": new_person["color"],"size": new_person["size"]}})
-    # return "Update Made"
+    new_person = {}
+    new_person["name"] = request.args.get('name')
+    new_person["production_year"] = int(request.args.get('production_year'))
+    new_person["price"] = float(request.args.get('price'))
+    new_person["color"] = int(request.args.get('color'))
+    new_person["size"] = int(request.args.get('size'))
 
-  
-    # Get JSON data from the POST request
-    new_person = request.get_json()
 
-    # Check if data is received correctly
-    print(new_person)
-
+    # the find_one returns the first document that matches your query criteria or None
     exists = mongo.db.products.find_one({"name": new_person["name"]})
     if exists is None:
         mongo.db.products.insert_one(new_person)
         return "Addition Made"
     else:
-        mongo.db.products.update_one({"name": new_person["name"]}, {"$set": {"id": new_person["id"],"production_year": new_person["production_year"],"price": new_person["price"],"color": new_person["color"],"size": new_person["size"]}})
-    return "Update Made"
+        mongo.db.products.update_one({"name": new_person["name"]}, {
+            "$set": {"production_year": new_person["production_year"],
+                     "price": new_person["price"], "color": new_person["color"], "size": new_person["size"]}})
+        return "Update Made"
 
-    
 
     # END CODE HERE
 
