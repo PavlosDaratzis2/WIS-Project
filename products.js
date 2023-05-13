d955341b939c7ed622e1fb0265c285428219ade0
PavlosDaratzis2 <pauldaratzis1997@gmail.com>
2023-05-13 12:34:50 +0300

    αλλαγες στο κωδικα html και javascript ετσι ωστε να περνουμε ορισμτα απο την φορμα το προβλημα ειναι οτι δεν γινονται reset το αροσματ στο τελος

--- products.js
+++ products.js
@@ -18,25 +18,42 @@ searchButtonOnClick = () => {
 
 productFormOnSubmit = (event) => {
     // BEGIN CODE HERE
+    event.preventDefault();
+
+    // Get form values
+    const name = document.getElementById("name");
+    const production_year = document.getElementById("year");
+    const price = document.getElementById("price");
+    const color = document.getElementById("fcolor");
+    const size = document.getElementById("size");
+
     const res = new XMLHttpRequest();
     res.open("POST", `http://127.0.0.1:5000/add-product`);
     res.onreadystatechange = () => {
         if (res.readyState == 4) {
             if (res.status == 200) {
                 alert(res.responseText);
+
+                // Clear form fields
+                name.value = '';
+                production_year.value = '';
+                price.value = '';
+                color.value = '';
+                size.value = '';
+                event.target.reset();
+
             }
         }
     };
-     // same for the project
+
     res.setRequestHeader("Content-Type", "application/json");
     res.send(JSON.stringify({
-        "name":"Paper A10",
-        "production_year":2010,
-        "price": 10,
-        "color": 1,
-        "size": 2
+        name: name.value,
+        production_year: parseInt(production_year.value),
+        price: parseFloat(price.value),
+        color: parseInt(color.value),
+        size: parseInt(size.value)
     }))
-
 
     // END CODE HERE
 }
