15b4062e46236e6d95a442b817ab3720768df4c2
PavlosDaratzis2 <pauldaratzis1997@gmail.com>
2023-05-14 14:42:35 +0300

    εκτελεσιμο javascript

--- products.js
+++ products.js
@@ -18,14 +18,50 @@ searchButtonOnClick = () => {
 
 productFormOnSubmit = (event) => {
     // BEGIN CODE HERE
+
+//    // Get form values
+//    const name = document.getElementById("name");
+//    const production_year = document.getElementById("year");
+//    const price = document.getElementById("price");
+//    const color = document.getElementById("fcolor");
+//    const size = document.getElementById("size");
+//
+//    const res = new XMLHttpRequest();
+//    res.open("POST", `http://127.0.0.1:5000/add-product`);
+//    res.onreadystatechange = () => {
+//        if (res.readyState == 4) {
+//            if (res.status == 200) {
+//                alert(res.responseText);
+//
+//                // Clear form fields
+//                // name.innerHTML = "";
+//                // production_year.innerHTML = "";
+//                // price.innerHTML = "";
+//                // color.innerHTML = "";
+//                // size.innerHTML = "";
+//            }
+//        }
+//    };
+//
+//    res.setRequestHeader("Content-Type", "application/json");
+//    res.send(JSON.stringify({
+//        name: "Paper A8",
+//        production_year: "2006",
+//        price: "8",
+//        color: "4",
+//        size: "2"
+//    }))
+
+
+
     event.preventDefault();
 
     // Get form values
-    const name = document.getElementById("name");
-    const production_year = document.getElementById("year");
-    const price = document.getElementById("price");
-    const color = document.getElementById("fcolor");
-    const size = document.getElementById("size");
+    const name = document.getElementById("name").value;
+    const production_year = document.getElementById("year").value;
+    const price = document.getElementById("price").value;
+    const color = document.getElementById("fcolor").value;
+    const size = document.getElementById("size").value;
 
     const res = new XMLHttpRequest();
     res.open("POST", `http://127.0.0.1:5000/add-product`);
@@ -35,25 +71,24 @@ productFormOnSubmit = (event) => {
                 alert(res.responseText);
 
                 // Clear form fields
-                name.value = '';
-                production_year.value = '';
-                price.value = '';
-                color.value = '';
-                size.value = '';
-                event.target.reset();
-
+                document.getElementById("name").value = "";
+                document.getElementById("year").value = "";
+                document.getElementById("price").value = "";
+                document.getElementById("fcolor").value = "";
+                document.getElementById("size").value = "";
             }
         }
     };
 
     res.setRequestHeader("Content-Type", "application/json");
     res.send(JSON.stringify({
-        name: name.value,
-        production_year: parseInt(production_year.value),
-        price: parseFloat(price.value),
-        color: parseInt(color.value),
-        size: parseInt(size.value)
-    }))
+        name: name,
+        production_year: production_year,
+        price: price,
+        color: color,
+        size: size
+    }));
+
 
     // END CODE HERE
 }
